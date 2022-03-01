9a016026e3dae3fc5b3bae7eb6dd50405f451dcd
Adrián Vatai <vataiadrian@gmail.com>
2022-03-01 09:40:46 +0100

    Frontend: login átszervezése php-ra és egyeztetés az adatbázissal

--- weboldal/frontend/app.js
+++ weboldal/frontend/app.js
@@ -1,9 +1,9 @@
-var app= angular.module('pizzaApp',[]);
+var app= angular.module('ClickApp',[]);
 
 app.run(function($rootScope, $http){
     if (sessionStorage.getItem['uID']) {
         $rootScope.loggedIn=1;
-        $rootScope.userName=sessionStorage.getItem('uName');  
+        $rootScope.userName=sessionStorage.getItem('uName');
     }
     else{
         $rootScope.loggedIn=0;
--- weboldal/frontend/login.js
+++ weboldal/frontend/login.js
@@ -4,14 +4,19 @@ app.controller('loginCtrl', function($scope, $rootScope, dbFactory) {
     $scope.login=function () {
         
         if ($scope.nev==null || $scope.jelszo==null) {
-            console.log("asd "+$scope.nev);
-            console.log("asdf "+$scope.jelszo);
             alert('Nem adtad meg a belépési adatokat!');
             
         }
         else
         {
-            dbFactory.select('felhasznalok', 'nev="'+$scope.nev+'" AND jelszo="'+CryptoJS.SHA1($scope.jelszo)+'"')
+            $http({
+                method: "POST",
+                url: "./API/getOneRecord.php",
+                data: {
+                    'table': 'users',
+                    'felt': 'Email="' + $scope.email + '" AND jelszo="' + CryptoJS.SHA1($scope.passwd) + '"'
+                }
+            })
             .then(function (response) {
                 $scope.users=response.data;
                 if ($scope.users.length==0) {
@@ -19,22 +24,15 @@ app.controller('loginCtrl', function($scope, $rootScope, dbFactory) {
                 } else {
                     $rootScope.loggedIn=1;
                     sessionStorage.setItem('uID',angular.toJson($scope.users.ID));
-                    sessionStorage.setItem('uName',angular.toJson($scope.users.nev));
+                    sessionStorage.setItem('uName',angular.toJson($scope.users.Nev));
+                    sessionStorage.setItem('uMail',angular.toJson($scope.users.Email));
                     sessionStorage.setItem('uLoggedIn',angular.toJson($rootScope.loggedIn));
+                    sessionStorage.setItem('uJog',angular.toJson($scope.users.jogosultsag));
                     
-                    $rootScope.userName=$scope.users.nev;
-                    console.log("Belépés sikerült!");
-                    alert("Belépés sikerült!");
+                    $location.path('#!/');
+
                 }    
             })
         }
-    }
-
-    $scope.logout=function () {
-        sessionStorage.removeItem('uID');
-        sessionStorage.removeItem('uName');
-        sessionStorage.removeItem('uLoggedIn');
-        $rootScope.loggedIn=0;
-        $rootScope.userName="";
     }
 });
