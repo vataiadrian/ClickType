1349d71e5fe5fa0fd2ce919d347e7336900ef6f7
MrSakal <mr.sakal0123@gmail.com>
2022-03-03 22:06:10 +0100

    Weboldal átláthatóságának szerkesztése

--- weboldal/frontend/design/js/main.js
+++ weboldal/frontend/design/js/main.js
@@ -1,220 +1,74 @@
-var swiper = new Swiper(".mySwiper", {
-  autoHeight: true,
-  centeredSlides: true,
-  spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 8000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+/*--------------------------------------------------------------
+# Selector
+--------------------------------------------------------------*/
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
+  }
+}
 
-
-(function() {
-  "use strict";
-
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
+/*--------------------------------------------------------------
+# Listener
+--------------------------------------------------------------*/
+const on = (type, el, listener, all = false) => {
+  let selectEl = select(el, all)
+  if (selectEl) {
     if (all) {
-      return [...document.querySelectorAll(el)]
+      selectEl.forEach(e => e.addEventListener(type, listener))
     } else {
-      return document.querySelector(el)
+      selectEl.addEventListener(type, listener)
     }
   }
+}
 
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
+/*--------------------------------------------------------------
+# Scroll listener
+--------------------------------------------------------------*/
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
+
+/*--------------------------------------------------------------
+# Menu hattér áttetszősége
+--------------------------------------------------------------*/
+let selectHeader = select('#header')
+let selectTopbar = select('#topbar')
+if (selectHeader) {
+  const headerScrolled = () => {
+    if (window.scrollY > 100) {
+      selectHeader.classList.add('header-scrolled')
+      if (selectTopbar) {
+        selectTopbar.classList.add('topbar-scrolled')
+      }
+    } else {
+      selectHeader.classList.remove('header-scrolled')
+      if (selectTopbar) {
+        selectTopbar.classList.remove('topbar-scrolled')
       }
     }
   }
+  window.addEventListener('load', headerScrolled)
+  onscroll(document, headerScrolled)
+}
 
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+/*--------------------------------------------------------------
+# Mobil nézet menu
+--------------------------------------------------------------*/
+on('click', '.mobile-nav-toggle', function(e) {
+  select('#navbar').classList.toggle('navbar-mobile')
+  this.classList.toggle('bx-menu')
+  this.classList.toggle('bx-x')
+})
 
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
-  let selectHeader = select('#header')
-  let selectTopbar = select('#topbar')
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
-        if (selectTopbar) {
-          selectTopbar.classList.add('topbar-scrolled')
-        }
-      } else {
-        selectHeader.classList.remove('header-scrolled')
-        if (selectTopbar) {
-          selectTopbar.classList.remove('topbar-scrolled')
-        }
-      }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
-  }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bx-menu')
-    this.classList.toggle('bx-x')
+/*--------------------------------------------------------------
+# Animáció
+--------------------------------------------------------------*/
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease',
   })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bx-menu')
-        navbarToggle.classList.toggle('bx-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
-
-  
-
-  /**
-   * Preloader
-   */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
-
-  /**
-   * Menu isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let menuContainer = select('.menu-container');
-    if (menuContainer) {
-      let menuIsotope = new Isotope(menuContainer, {
-        itemSelector: '.menu-item',
-        layoutMode: 'fitRows'
-      });
-
-      let menuFilters = select('#menu-flters li', true);
-
-      on('click', '#menu-flters li', function(e) {
-        e.preventDefault();
-        menuFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        menuIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        menuIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate glightbox 
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
-
-  /**
-   * Initiate gallery lightbox 
-   */
-  const galleryLightbox = GLightbox({
-    selector: '.gallery-lightbox'
-  });
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  });
-
-})()
+});
